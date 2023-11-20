3e7389fac8508ac231510e1d7637256414ce8866
Kostya <pavli4enko30@gmail.com>
2023-11-20 12:55:26 +0100

    update layout with header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="default-layout dark-theme">
-    <TopMenu />
+  <div class="default-layout">
+    <header>
+      <TopMenu />
+    </header>
     <nuxt-page />
     <footer>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
+      <!-- <div>&copy; {{ new Date().getFullYear() }}</div> -->
       <BottomNavigation />
     </footer>
   </div>
@@ -25,4 +27,28 @@ const drawer = ref(null)
 
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.default-layout {
+  background-color: var(--md-sys-color-surface);
+  min-height: 100vh;
+}
+
+header {
+  display: none;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  /* Height of the footer*/
+  height: fit-content;
+  background: grey;
+}
+
+@media (min-width: $laptop-breakpoint) {
+  header {
+    display: block;
+  }
+}
+</style>
